4709cf061d13f0232609fcdfec918306eacf665c
lili <80553571+livinhe@users.noreply.github.com>
2023-05-10 11:14:05 -0300

    Update
    Clean php

--- aula-05/processa.php
+++ aula-05/processa.php
@@ -1,29 +1,51 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-  // DC2 2023/1: processamento de dados de formulários
-  // copiem este arquivo para a pasta de destino e 
-  // definam "processa.php" como atributo action de form
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Formulário - Post Respostas</title>
+</head>
 
-  // exibe dados post
+<main class="respostas">
+  <?php
   if (isset($_POST['form_post'])) {
-    
-    printf('<h1>dados post</h1>');
-    // laço sobre vetor
-    foreach($_POST as $nome => $valor) {
-      printf("%s: %s<br>",$nome,$valor);
-    }
-    
-  } 
-  else {
 
-    // exibe dados get
-    
-    printf('<h1>dados get</h1>');
-    // laço sobre vetor
-    foreach($_GET as $nome => $valor) {
-      printf("%s: %s<br>",$nome,$valor);
-    }
-    
-  }
+    printf('<h1>Dados - Formulário </h1>');
+  ?>
 
-?>
+    <ul>
+
+      <?php
+          foreach ($_POST as $nome => $valor) {
+            $nome_limpo = ucwords(preg_replace("/[^a-zA-Z]+/", " ", $nome));
+            $label_name = "<li><strong>{$nome_limpo}:</strong>";
+
+
+            if ($nome == 'disciplinas') {
+              printf("{$label_name} %s</li>", implode(', ', $_POST['disciplinas']));
+            } else if ($nome == 'form_post') {
+              printf('');
+            } else if ($nome == 'genero') {
+              if($valor == 'none') {
+                printf("{$label_name} %s</li>", 'Prefiro não responder');
+              } else {
+                printf("{$label_name} %s</li>", ucwords($valor));
+              }
+            } else if ($nome == 'data_nascimento') {
+              $data_array = explode('-', $valor); 
+              printf("{$label_name} %s/%s/%s</li>", $data_array[2],$data_array[1], $data_array[0]);
+            }
+            else {
+              printf("{$label_name} %s</li>", $valor);
+            }
+          }
+        }
+      ?>
+
+    </ul>
+</main>
+
+<?php
